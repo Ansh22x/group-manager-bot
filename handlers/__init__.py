b8f7def8b15c1fc2ccd4c087d39b50b462831e93
Ansh22x <moureansh@gmail.com>
2026-08-19 10:28:15 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -8,11 +8,6 @@ from handlers.ai_chat_handler import AIChatHandler
 from handlers.captcha_handler import CaptchaHandler
 from handlers.economy_handler import EconomyHandler
 from handlers.media_handler import MediaHandler
-from telegram.ext import Application, CommandHandler
-from handlers.ai_chat import ask_command
-
-def register_handlers(app: Application):
-    app.add_handler(CommandHandler("ask", ask_command)
 
 def register_handlers(app: Application):
     """Instantiates and registers all BaseHandler subclasses to the bot application"""
